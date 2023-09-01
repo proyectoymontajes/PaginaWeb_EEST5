38040fa2ba0d4b8622a13ba87626b564938724a3
wilvers <juan.ignacio.wilvers@gmail.com>
2023-09-01 18:05:58 -0300

    proyects

--- src/pages/Proyects/index.tsx
+++ src/pages/Proyects/index.tsx
@@ -1,8 +1,51 @@
-
+import CardProyect from '../../components/CardProyect'
+import './Proyects.css'
 const Proyects = () => {
-  return (
-    <div>Proyects</div>
-  )
+    return (
+        <div className='container-proyectsPage' >
+            <div className="container-proyects">
+                <h2>Robotica</h2>
+
+                <div className="list-proyects">
+                    <CardProyect
+                        img='https://th.bing.com/th/id/OIP.x88bSGJ2CtEHujqH2s0XbAHaE7?pid=ImgDet&rs=1'
+                        alt='aaaa'
+                        title='no se '
+                    />
+                    <CardProyect
+                        img='https://th.bing.com/th/id/OIP.x88bSGJ2CtEHujqH2s0XbAHaE7?pid=ImgDet&rs=1'
+                        alt='aaaa'
+                        title='no se '
+                    />
+                    <CardProyect
+                        img='https://th.bing.com/th/id/OIP.x88bSGJ2CtEHujqH2s0XbAHaE7?pid=ImgDet&rs=1'
+                        alt='aaaa'
+                        title='no se '
+                    />
+                    <CardProyect
+                        img='https://th.bing.com/th/id/OIP.x88bSGJ2CtEHujqH2s0XbAHaE7?pid=ImgDet&rs=1'
+                        alt='aaaa'
+                        title='no se '
+                    />
+                    <CardProyect
+                        img='https://th.bing.com/th/id/OIP.x88bSGJ2CtEHujqH2s0XbAHaE7?pid=ImgDet&rs=1'
+                        alt='aaaa'
+                        title='no se '
+                    />
+                    <CardProyect
+                        img='https://th.bing.com/th/id/OIP.x88bSGJ2CtEHujqH2s0XbAHaE7?pid=ImgDet&rs=1'
+                        alt='aaaa'
+                        title='no se '
+                    />
+                    <CardProyect
+                        img='https://th.bing.com/th/id/OIP.x88bSGJ2CtEHujqH2s0XbAHaE7?pid=ImgDet&rs=1'
+                        alt='aaaa'
+                        title='no se '
+                    />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Proyects
