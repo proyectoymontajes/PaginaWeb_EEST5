e2c2b1e2a97d20ff8a36fefd52b5b0aa1042c143
wilvers <juan.ignacio.wilvers@gmail.com>
2023-09-04 11:43:16 -0300

    update search proyect

--- src/pages/Proyects/index.tsx
+++ src/pages/Proyects/index.tsx
@@ -2,12 +2,23 @@ import { items } from '../../data/dataProyects';
 import './Proyects.css'
 import { ListProyects } from './components/ListProyects'
 
+import SearchIcon from '@mui/icons-material/Search';
+import TuneIcon from '@mui/icons-material/Tune';
+
 const Proyects = () => {
     return (
         <div className='container-proyectsPage' >
+            <div className="menu-proyects">
+                
+                <TuneIcon className='settings-icon' fontSize='large' />
+                <form action="">
+                    <input type="text" name="" id="" />
+                    <SearchIcon className='search-icon' />
+                </form>
+            </div>
             {
-                items.map( ({ title, items})=> (
-                    <ListProyects key={title} category ={title} items={items} />
+                items.map(({ title, items }) => (
+                    <ListProyects key={title} category={title} items={items} />
                 ))
             }
         </div>
