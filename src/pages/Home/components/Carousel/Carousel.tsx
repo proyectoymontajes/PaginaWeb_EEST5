5deadc585b5ad028b50a60118a48016163cb761c
wilvers <juan.ignacio.wilvers@gmail.com>
2023-09-28 18:48:33 -0300

    create components in carousel and update imgs

--- src/pages/Home/components/Carousel/Carousel.tsx
+++ src/pages/Home/components/Carousel/Carousel.tsx
@@ -1,4 +1,4 @@
-import frenteEscuela from "../../../../assets/carousel/frente-escuela.jpg";
+import bonaerenses2023 from "../../../../assets/carousel/bonaerenses2023.jpg";
 import frenteEscuela2 from "../../../../assets/carousel/frente-escuela2.jpg";
 import patio from "../../../../assets/carousel/patio.jpg";
 import robotica from "../../../../assets/carousel/robotica.jpg";
@@ -9,77 +9,70 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 import './Carousel.css'
+import Item from "./components/item";
 
 export const Carousel = () => {
 
-    const scrollToSection = (section:string) => {
+    const scrollToSection = (section: string) => {
         const sectionScroll = document.getElementById(section); // Reemplaza 'targetSection' con el ID de la sección a la que deseas desplazarte.
-        
+
         if (sectionScroll) {
-          sectionScroll.scrollIntoView({
-            behavior: 'smooth', // Esto activa la animación de desplazamiento suave.
-            block: 'start',     // Esto alineará la parte superior de la sección con la parte superior de la ventana.
-          });
+            sectionScroll.scrollIntoView({
+                behavior: 'smooth', // Esto activa la animación de desplazamiento suave.
+                block: 'start',     // Esto alineará la parte superior de la sección con la parte superior de la ventana.
+            });
         }
-      };
+    };
 
     return (
         <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
             <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img src={frenteEscuela}
-                        className="d-block w-100"
-                        alt="..."/>
-                    <div className="overlay">
-                        <h1 className="container__typing">
-                            Escuela Técnica N°5 Temperley.
-                        </h1>
-                    </div>
-                </div>
-                <div className="carousel-item">
-                    <img src={frenteEscuela2}
-                        className="d-block w-100"
-                        alt="..."/>
+                <Item
+                    active={true}
+                    src={bonaerenses2023}
+                >
+                    <h1
+                        className="container__typing"
+                        // style={{ marginTop: '0' }}
+                    >
+                        Escuela Técnica N°5 Temperley.
+                    </h1>
+                </Item>
+
+                <Item src={frenteEscuela2} >
                     <div className="overlay-proyectos">
                         <h1 className="container__proyectos">
                             CONOCÉ ACERCA DE NUESTROS PROYECTOS
                         </h1>
                         <button className="button-informacion" onClick={() => scrollToSection('proyects')}>MÁS INFORMACIÓN</button>
                     </div>
-                </div>
-                <div className="carousel-item">
-                    <img src={patio}
-                        className="d-block w-100"
-                        alt="..."/>
-                         <div className="overlay-proyectos">
+                </Item>
+
+                <Item src={patio}>
+                    <div className="overlay-proyectos">
                         <h1 className="container__proyectos">
                             CONOCÉ ACERCA DE NOSOTROS
                         </h1>
                         <button className="button-informacion" onClick={() => scrollToSection('about-us')}>MÁS INFORMACIÓN</button>
                     </div>
-                </div>
-                <div className="carousel-item">
-                    <img src={robotica}
-                        className="d-block w-100"
-                        alt="..."/>
-                </div>
-                <div className="carousel-item">
-                    <img src={robotica2}
-                        className="d-block w-100"
-                        alt="..."/>
-                </div>
+                </Item>
+
+                <Item src={robotica} />
+
+                <Item src={robotica2} />
+
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
                 <div className="contain-button-carousel">
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                 </div>
-                <span className="visually-hidden">Previous</span>
+                {/* <span className="visually-hidden">Previous</span> */}
             </button>
             <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
                 <div className="contain-button-carousel">
                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
                 </div>
-                <span className="visually-hidden">Next</span>
+                {/* <span className="visually-hidden">Next</span> */}
             </button>
         </div>
     )
