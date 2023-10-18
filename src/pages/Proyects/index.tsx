34a630dbfe65e64e6ad8a16135e2d32c1747c80a
Matias Rolón <matiasrolon1301@gmail.com>
2023-10-18 18:52:42 -0300

    fixed error scroll

--- src/pages/Proyects/index.tsx
+++ src/pages/Proyects/index.tsx
@@ -3,8 +3,14 @@ import { ListFilterst } from './components/ListFilterst';
 import { ListContextProvider } from './context/ListContext';
 import ProyectsCategory from './components/ProyectsCategory';
 import { SearchBar } from './components/SearchBar';
+import { useEffect } from 'react';
 
 const Proyects = () => {
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+      }, []);
+
     return (
         <ListContextProvider>
             <div className='container-proyectsPage' >
