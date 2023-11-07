728f6005f795107f57a56b044b373289049b8313
Matias Rolón <matiasrolon1301@gmail.com>
2023-11-06 22:30:37 -0300

    fix proyects on home

--- src/pages/Home/components/Proyectos/Proyectos.tsx
+++ src/pages/Home/components/Proyectos/Proyectos.tsx
@@ -14,9 +14,9 @@ export const Proyectos = () => {
             <h1 className="title-proyects" id='proyects'>PROYECTOS</h1>
             <div className="contain-proyects">
                 <div className='contain-proyects'>
-                    <CardProyect img='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdVkL6idPJwyGYKGwjN-PsNKXGvc2XgCXcxL7wV6kpdQ73mriIQmm1WRRHqpsF8jvZcbw&usqp=CAU' title='Smartwatch'/>
-                    <CardProyect img='https://ingenieria.uncuyo.edu.ar/cache/2017-001-dscn3377_800_900.jpg' title='Brazo robotico'/>
-                    <CardProyect img='https://matias-rolon.github.io/pagina_IoT.github.io/WhatsApp%20Image%202022-11-24%20at%2014.13.13.jpeg' title='Internet de las cosas'/>
+                    <CardProyect img='https://github.com/matias-rolon/img-proyectos/blob/main/auto-smart.png?raw=true' title='AUTO SMART'/>
+                    <CardProyect img='https://github.com/matias-rolon/img-proyectos/blob/main/ara%C3%B1a.png?raw=true' title='ARAÑA ROBOTICA'/>
+                    <CardProyect img='https://matias-rolon.github.io/pagina_IoT.github.io/WhatsApp%20Image%202022-11-24%20at%2014.13.13.jpeg' title='IOT'/>
                 </div>
             </div>
             <div className='read-more' onClick={ () => navigate("/proyects") }>
