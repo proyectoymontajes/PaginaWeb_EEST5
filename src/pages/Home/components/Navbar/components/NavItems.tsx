85a8e5f6e00c94c88593a2f0d296eb3b532a5b95
Matias Rolón <matiasrolon1301@gmail.com>
2023-11-07 20:47:36 -0300

    fix bugs navbar

--- src/pages/Home/components/Navbar/components/NavItems.tsx
+++ src/pages/Home/components/Navbar/components/NavItems.tsx
@@ -1,37 +1,30 @@
 import { NavbarItem } from '@nextui-org/react';
 import { ItemDropdown } from './ItemDropdown';
 import { NavLink } from 'react-router-dom';
+import '../Navbar.css';
 
-import '../Navbar.css'
-
-export const NavItems = () => {
-    
-    const scrollToSection = (section: string) => {
-        const sectionScroll = document.getElementById(section);
-
-        if (sectionScroll) {
-            sectionScroll.scrollIntoView({
-                behavior: 'smooth', 
-                block: 'start',     
-            });
-        }
-    };
-
+export const NavItems = ({ closeMenu }:{closeMenu: () => void}) => {
     return (
         <>
             <NavbarItem>
-                <NavLink className="item link-navbar" to="/" >Inicio</NavLink>
+                <NavLink className="item" to="/" onClick={closeMenu}>
+                    Inicio
+                </NavLink>
             </NavbarItem>
 
             <NavbarItem>
-                <NavLink className="item" to="/proyects">Proyectos</NavLink>
+                <NavLink className="item" to="/proyects" onClick={closeMenu}>
+                    Proyectos
+                </NavLink>
             </NavbarItem>
 
             <NavbarItem>
-                <NavLink className="item" to="/ "onClick={() => scrollToSection('about-us')}>Sobre nosotros</NavLink>
+                <NavLink className="item" to="/about" onClick={closeMenu}>
+                    Sobre nosotros
+                </NavLink>
             </NavbarItem>
 
             <ItemDropdown />
         </>
-    )
+    );
 }
