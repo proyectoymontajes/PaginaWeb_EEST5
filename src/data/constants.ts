e77634689cb0ffdcb941217ff6c4d41d5021f1a4
wilvers <juan.ignacio.wilvers@gmail.com>
2023-11-14 18:37:29 -0300

    update constants and styles

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -2,31 +2,35 @@ import { bona1, bona2, bona3 } from "../assets";
 
 
 export const achievement = [
-    {
-      title: "Medalla de oro ⭐",
-      icon : bona1,
-      iconBg: "#383E56",
-      date: "2019",
-      points: [
-        "Campeones bonaerenses de robotica 2019",
-      ],
-    },
-    {
-      title: "Medalla de oro ⭐⭐",
-      icon : bona2,
-      iconBg: "#E6DEDD",
-      date: "2022",
-      points: [
-        "Campeones bonaerenses de robotica 2022",
-      ],
-    },
-    {
-        title: "Medalla de oro ⭐⭐⭐",
-        icon : bona3,
-        iconBg: "#E6DEDD",
-        date: "2023",
-        points: [
-          "Campeones bonaerenses de robotica 2023",
-        ],
-      },
-  ];
+  {
+    title: "Medalla de oro ⭐",
+    icon: bona1,
+    iconBg: "#383E56",
+    date: "2019",
+    points: [
+      "Campeones bonaerenses de robotica 2019",
+      "📌 Mar del Plata",
+    ],
+  },
+  {
+    title: "Medalla de oro ⭐⭐",
+    icon: bona2,
+    iconBg: "#E6DEDD",
+    date: "2022",
+    points: [
+      "Campeones bonaerenses de robotica 2022",
+      "📌 Mar del Plata",
+
+    ],
+  },
+  {
+    title: "Medalla de oro ⭐⭐⭐",
+    icon: bona3,
+    iconBg: "#E6DEDD",
+    date: "2023",
+    points: [
+      "Campeones bonaerenses de robotica 2023",
+      "📌 Mar del Plata",
+    ],
+  },
+];
