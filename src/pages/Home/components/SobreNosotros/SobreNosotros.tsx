fcc0735233808c259a62bbd86bcae7c6d5eb854f
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-11-13 23:14:33 -0300

    home title

--- src/pages/Home/components/SobreNosotros/SobreNosotros.tsx
+++ src/pages/Home/components/SobreNosotros/SobreNosotros.tsx
@@ -2,11 +2,11 @@ import '../../styles.css'
 
 export const SobreNosotros = () => {
     return (
-        <>
+        <div>
             <h1 className="title-about-us-section" id="about-us">SOBRE NOSOTROS</h1>
             <div className='body-about-us'>
                 <p>Somos el taller del departamento de electrónica del Industrial de Temperley. Es un espacio para desarrollar proyectos de electrónica vinculando el contenido de las materias con las nuevas tecnologias como robótica, inteligencia artificial, cloud computing, software, etc. Te invitamos a explorar y conocer más sobre los proyectos. ¡Gracias por visitar nuestra página web!</p>
             </div>
-        </>
+        </div>
     )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,27 +1,13 @@
 import {Proyectos} from './components/Proyectos/Proyectos';
 import {SobreNosotros} from './components/SobreNosotros/SobreNosotros';
-import {Canvas} from 'react-three-fiber';
-import {Robot} from '../../components/3d/Robot';
-import {Suspense, useEffect, useState} from 'react';
-import {OrbitControls} from '@react-three/drei';
 
 const Home = () => {
-    const [zoom, setZoom] = useState(4);
-
-    useEffect(() => {
-        const isMobile = window.matchMedia('(max-width: 1270px)');
-
-        if (isMobile.matches) {
-            setZoom(1);
-        } else {
-            setZoom(4);
-        }
-    }, []);
 
     return (
         <>
             <div className="container-home" >
-                
+                <h1 className="title-home">Taller de&nbsp;</h1>
+                <h1 className="title-home-sub">Electronica</h1>
             </div>
             <Proyectos/>
             <SobreNosotros/>
